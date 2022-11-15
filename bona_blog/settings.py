f44cc226f13fd58d3efa42c23cb399153ec3437a
Ubuntu <azureuser@linuxtest1.eaef4x4xry5uflp0igfa1vmbgc.bx.internal.cloudapp.net>
2022-11-15 00:13:47 +0000

    trying to fix settings

--- bona_blog/settings.py
+++ bona_blog/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
+    "whitenoise.runserver_nostatic",
     "django.contrib.staticfiles",
     # Third Party Apps.
     "django_filters",
@@ -141,12 +142,11 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 # Following settings only make sense on production and may break development environments.
-if not DEBUG:  # Tell Django to copy statics to the `staticfiles` directory
     # in your application directory on Render.
-    STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
     # Turn on WhiteNoise storage backend that takes care of compressing static files
     # and creating unique names for each version so they can safely be cached forever.
-    STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 
 # Media files (User uploaded images)
 MEDIA_URL = "/media/"
